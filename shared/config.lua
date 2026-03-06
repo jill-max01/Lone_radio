14c39e1a2f6c02f155187821c9614358c9239144
HaswanthRaj <155101687+lone17k@users.noreply.github.com>
2026-03-06 16:50:48 +0530

    migration to reactjs and v2

--- shared/config.lua
+++ shared/config.lua
@@ -7,8 +7,10 @@ Config.command = "radio"
 --Some url/streaming links maybe not supported
 
 Config.Radios = {
-    { name = "Vanavil FM", url = "https://s7.yesstreaming.net:9000/stream" },
-    { name = "NCS Music", url = "https://www.youtube.com/watch?v=MsSrIlOi81o" },
-   -- { name = "Name that will be displayed" url = "streaming url of the radio"},
-    -- Add more radios as needed
+    { name = "Vanavil FM", url = "https://s7.yesstreaming.net:9000/stream", freq = 88.5 },
+    { name = "NCS Music", url = "https://www.youtube.com/watch?v=MsSrIlOi81o", freq = 92.1 },
+    { name = "Los Santos Rock Radio", url = "", freq = 97.8 },
+    { name = "Non-Stop-Pop FM", url = "", freq = 100.7 },
+    { name = "West Coast Classics", url = "", freq = 103.2 },
+    { name = "Radio Los Santos", url = "", freq = 106.5 },
 }
